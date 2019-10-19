b65670aad58aa1d05c8c8914556da8536e22280e
heavenly-zy <1457715339@qq.com>
2019-10-20 00:51:04 +0800

    初步优化代码

--- todo.js
+++ todo.js
@@ -3,8 +3,10 @@ var fs = require('fs'); // file system 文件系统
 const verb = process.argv[2] // add
 const content = process.argv[3] // 砸瓦鲁多
 const editContent = process.argv[4]
-if (verb === 'add') {
-    fs.stat('C:\\Users\\asus\\Desktop\\node-todo-demo\\db', function (err, stat) {
+
+switch (verb) {
+    case 'add':
+        fs.stat('C:\\Users\\asus\\Desktop\\node-todo-demo\\db', function (err, stat) {
         if (err == null) { // 文件存在
             const fileContent = fs.readFileSync('C:\\Users\\asus\\Desktop\\node-todo-demo\\db').toString()
             const list = JSON.parse(fileContent) // 反序列化
@@ -25,34 +27,41 @@ if (verb === 'add') {
             console.log('Some other error: ', err.code)
         }
     })
-} else if (verb === 'list') {
-    const fileContent = fs.readFileSync('C:\\Users\\asus\\Desktop\\node-todo-demo\\db').toString()
-    const list = JSON.parse(fileContent) // 反序列化
-    console.log(list)
-} else if (verb === 'delete') {
-    const fileContent = fs.readFileSync('C:\\Users\\asus\\Desktop\\node-todo-demo\\db').toString()
-    const list = JSON.parse(fileContent)
-    const n = content // 用户从 1 开始删除
-    list.splice(n - 1, 1)
-    console.log(list)
-    fs.writeFileSync('C:\\Users\\asus\\Desktop\\node-todo-demo\\db', JSON.stringify(list))
-} else if (verb === 'done') {
-    const fileContent = fs.readFileSync('C:\\Users\\asus\\Desktop\\node-todo-demo\\db').toString()
-    const list = JSON.parse(fileContent)
-    const n = content
-    list[n - 1][1] = true
-    console.log(list)
-    fs.writeFileSync('C:\\Users\\asus\\Desktop\\node-todo-demo\\db', JSON.stringify(list))
-} else if(verb === 'edit'){
-    const fileContent = fs.readFileSync('C:\\Users\\asus\\Desktop\\node-todo-demo\\db').toString()
-    const list = JSON.parse(fileContent)
-    const n = content
-    list[n - 1][0] = editContent
-    console.log(list)
-    fs.writeFileSync('C:\\Users\\asus\\Desktop\\node-todo-demo\\db', JSON.stringify(list))
-} else {
-    console.log('你的动词是：' + verb)
-    console.log('我不知道你想干啥')
+        break;
+    case 'list':
+        const fileContent = fs.readFileSync('C:\\Users\\asus\\Desktop\\node-todo-demo\\db').toString()
+        const list = JSON.parse(fileContent) // 反序列化
+        console.log(list)
+        break;
+    case 'delete':
+        const fileContent = fs.readFileSync('C:\\Users\\asus\\Desktop\\node-todo-demo\\db').toString()
+        const list = JSON.parse(fileContent)
+        const n = content // 用户从 1 开始删除
+        list.splice(n - 1, 1)
+        console.log(list)
+        fs.writeFileSync('C:\\Users\\asus\\Desktop\\node-todo-demo\\db', JSON.stringify(list))
+        break;
+    case 'done':
+        const fileContent = fs.readFileSync('C:\\Users\\asus\\Desktop\\node-todo-demo\\db').toString()
+        const list = JSON.parse(fileContent)
+        const n = content
+        list[n - 1][1] = true
+        console.log(list)
+        fs.writeFileSync('C:\\Users\\asus\\Desktop\\node-todo-demo\\db', JSON.stringify(list))
+        break;
+    case 'edit':
+        const fileContent = fs.readFileSync('C:\\Users\\asus\\Desktop\\node-todo-demo\\db').toString()
+        const list = JSON.parse(fileContent)
+        const n = content
+        list[n - 1][0] = editContent
+        console.log(list)
+        fs.writeFileSync('C:\\Users\\asus\\Desktop\\node-todo-demo\\db', JSON.stringify(list))
+        break;
+    default:
+        console.log('你的动词是：' + verb)
+        console.log('我不知道你想干啥')
+        break;
 }
+
 
 
