3cec7e7aab54c4a683ee336a187a89cfa0ac6c1c
heavenly-zy <1457715339@qq.com>
2019-10-20 15:26:35 +0800

    优化代码完成

--- todo.js
+++ todo.js
@@ -1,61 +1,58 @@
 var fs = require('fs'); // file system 文件系统
 
 const verb = process.argv[2] // add
-const content = process.argv[3] // 砸瓦鲁多
+const content = process.argv[3] // 任务内容
 const editContent = process.argv[4]
+const dbPath = 'C:\\Users\\asus\\Desktop\\node-todo-demo\\db'
+
+let list, n
 
 switch (verb) {
     case 'add':
-        fs.stat('C:\\Users\\asus\\Desktop\\node-todo-demo\\db', function (err, stat) {
-        if (err == null) { // 文件存在
-            const fileContent = fs.readFileSync('C:\\Users\\asus\\Desktop\\node-todo-demo\\db').toString()
-            const list = JSON.parse(fileContent) // 反序列化
+        fs.stat(dbPath, function (err, stat) {
+            if (err == null) { // 文件存在
+                list = fetchFromDb()
 
-            const task = content // 获取任务
-            list.push([task, false]) // 存入任务到 list
-            fs.writeFileSync('C:\\Users\\asus\\Desktop\\node-todo-demo\\db', JSON.stringify(list)) // 序列化并存入数据库
-            console.log(list)
-        } else if (err.code === 'ENOENT') { // 文件不存在
-            fs.writeFileSync('C:\\Users\\asus\\Desktop\\node-todo-demo\\db', '') // 序列化
-            const list = []
+                addTask(list, content)
+                saveToDb(list)
+                displayList(list)
+            } else if (err.code === 'ENOENT') { // 文件不存在
+                fs.writeFileSync(dbPath, '') // 序列化
+                list = []
 
-            const task = content
-            list.push([task, false])
-            fs.writeFileSync('C:\\Users\\asus\\Desktop\\node-todo-demo\\db', JSON.stringify(list)) // 序列化
-            console.log(list)
-        } else { // 其他
-            console.log('Some other error: ', err.code)
-        }
-    })
+
+                addTask(list, content)
+                saveToDb(list) // 序列化
+                displayList(list)
+            } else { // 其他
+                console.log('Some other error: ', err.code)
+            }
+        })
         break;
     case 'list':
-        const fileContent = fs.readFileSync('C:\\Users\\asus\\Desktop\\node-todo-demo\\db').toString()
-        const list = JSON.parse(fileContent) // 反序列化
-        console.log(list)
+        list = fetchFromDb()
+        displayList(list)
         break;
     case 'delete':
-        const fileContent = fs.readFileSync('C:\\Users\\asus\\Desktop\\node-todo-demo\\db').toString()
-        const list = JSON.parse(fileContent)
-        const n = content // 用户从 1 开始删除
-        list.splice(n - 1, 1)
-        console.log(list)
-        fs.writeFileSync('C:\\Users\\asus\\Desktop\\node-todo-demo\\db', JSON.stringify(list))
+        list = fetchFromDb()
+        n = content // 用户从 1 开始删除
+        removeTask(list, n)
+        displayList(list)
+        saveToDb(list)
         break;
     case 'done':
-        const fileContent = fs.readFileSync('C:\\Users\\asus\\Desktop\\node-todo-demo\\db').toString()
-        const list = JSON.parse(fileContent)
-        const n = content
-        list[n - 1][1] = true
-        console.log(list)
-        fs.writeFileSync('C:\\Users\\asus\\Desktop\\node-todo-demo\\db', JSON.stringify(list))
+        list = fetchFromDb()
+        n = content
+        markTaskAsDone(list, n)
+        displayList(list)
+        saveToDb(list)
         break;
     case 'edit':
-        const fileContent = fs.readFileSync('C:\\Users\\asus\\Desktop\\node-todo-demo\\db').toString()
-        const list = JSON.parse(fileContent)
-        const n = content
-        list[n - 1][0] = editContent
-        console.log(list)
-        fs.writeFileSync('C:\\Users\\asus\\Desktop\\node-todo-demo\\db', JSON.stringify(list))
+        list = fetchFromDb()
+        n = content
+        editTask(list, n, editContent)
+        displayList(list)
+        saveToDb(list)
         break;
     default:
         console.log('你的动词是：' + verb)
@@ -63,5 +60,28 @@ switch (verb) {
         break;
 }
 
+function saveToDb(list) { // 存入数据库
+    fs.writeFileSync(dbPath, JSON.stringify(list)) // 序列化
+}
+function fetchFromDb() { // 读取数据库
+    const fileContent = fs.readFileSync(dbPath).toString()
+    list = JSON.parse(fileContent) // 反序列化
+    return list
 
+}
+function displayList(list) { // 列出所有任务
+    console.log(list)
+}
+function addTask(list, content) { // 存入任务到 list
+    list.push([content, false])
+}
+function removeTask(list, n) { // 删除任务
+    list.splice(n - 1, 1)
+}
+function markTaskAsDone(list, n) { // 标记任务完成(true)
+    list[n - 1][1] = true
+}
+function editTask(list, n, newContent) { // 编辑任务内容
+    list[n - 1][0] = newContent
+}
 
